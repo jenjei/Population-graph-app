43d54f1406a9799f5b0a92624f4340d4c0a9bd96
Jenni Ylisirniö <jenniylisirnio@Jennis-MacBook-Pro.local>
2020-04-06 14:00:54 +0300

    Lisäsin funktion fetchData, joka hakee halutut tiedot world bankilta.

--- myChart.js
+++ myChart.js
@@ -3,4 +3,19 @@ function logCountryCode() {
     var countryCode = document.getElementById('country').value;
     console.log(countryCode);
 }
-document.getElementById('renderBtn').addEventListener('click', logCountryCode);
+document.getElementById('renderBtn').addEventListener('click', fetchData);
+
+async function fetchData() {
+    var countryCode = document.getElementById('country').value;
+    const indicatorCode = 'SP.POP.TOTL';  
+    const baseUrl = 'https://api.worldbank.org/v2/country/';
+    const url = baseUrl + countryCode + '/indicator/' + indicatorCode + '?format=json';
+    console.log('Fetching data from URL: ' + url);
+
+    var response = await fetch(url);
+
+    if (response.status == 200) {
+        var fetchedData = await response.json();
+        console.log(fetchedData);
+    }
+}
