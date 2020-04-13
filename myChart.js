4c527d8a248d45883b2dd6f00f41963237a8f153
Jenni Ylisirniö <jenniylisirnio@Jennis-MacBook-Pro.local>
2020-04-13 16:29:31 +0300

    Huomasin, että funktiot eivät olleet samalla juuritasolla.

--- myChart.js
+++ myChart.js
@@ -19,48 +19,49 @@ async function fetchData() {
         var countryName = getCountryName(fetchedData);
         renderChart(data, labels, countryName);
     }
-    function getValues(data) {
-        var vals = data[1].sort((a, b) => a.date - b.date).map(item => item.value);
-        return vals;
+}
+function getValues(data) {
+    var vals = data[1].sort((a, b) => a.date - b.date).map(item => item.value);
+    return vals;
+}
+
+function getLabels(data) {
+    var labels = data[1].sort((a, b) => a.date - b.date).map(item => item.date);
+    return labels;
+}
+
+function getCountryName(data) {
+    var countryName = data[1][0].country.value;
+    return countryName;
+}
+function renderChart(data, labels, countryName) {
+    var ctx = document.getElementById('myChart').getContext('2d');
+
+    if (currentChart) {
+        currentChart.destroy();
     }
 
-    function getLabels(data) {
-        var labels = data[1].sort((a, b) => a.date - b.date).map(item => item.date);
-        return labels;
-    }
-
-    function getCountryName(data) {
-        var countryName = data[1][0].country.value;
-        return countryName;
-    }
-    function renderChart(data, labels, countryName) {
-        var ctx = document.getElementById('myChart').getContext('2d');
-
-        if (currentChart) {
-            currentChart.destroy();
-        }
-
-        // Draw new chart
-        currentChart = new Chart(ctx, {
-            type: 'line',
-            data: {
-                labels: labels,
-                datasets: [{
-                    label: 'Population, ' + countryName,
-                    data: data,
-                    borderColor: 'rgba(75, 192, 192, 1)',
-                    backgroundColor: 'rgba(97, 166, 28, 0.5)',
+    // Draw new chart
+    currentChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Population, ' + countryName,
+                data: data,
+                borderColor: 'rgba(75, 192, 192, 1)',
+                backgroundColor: 'rgba(97, 166, 28, 0.5)',
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
                 }]
-            },
-            options: {
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero: true
-                        }
-                    }]
-                }
             }
-        });
-    }
+        }
+    });
+}
 }
